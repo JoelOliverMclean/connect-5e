0ac2c1ca97aac0162aa8367fc62af9dfe70347d1
Joel Oliver Mclean <joelolivermclean@gmail.com>
2025-02-27 16:23:56 +0000

    add logs to auth

--- auth.js
+++ auth.js
@@ -3,12 +3,15 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import Google from "next-auth/providers/google";
 import db from "@/db";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  console.log("AUTH_GOOGLE_ID:", process.env.AUTH_GOOGLE_ID);
+  return {
+    adapter: DrizzleAdapter(db),
+    providers: [
+      Google({
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      }),
+    ],
+  };
 });
